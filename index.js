1d6b68e75f7df37f6c3685c15da679efc6176891
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-07-08 21:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var newArray = []
+for ( let i = 0; i < musicians.length; i++) {
+  newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+return newArray
+}
+
+function johnLennonFacts(array) {
+  let counter = 0
+  var newArray = []
+  while (counter < array.length) {
+    newArray.push(`${array[counter]}!!!`)
+      counter++
+  }
+  return newArray
+}
